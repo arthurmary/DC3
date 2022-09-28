dac8f8bf06fcb8c6c7d5d3538bee2e29c1a73569
arthurmary <113183213+arthurmary@users.noreply.github.com>
2022-09-28 15:03:18 +0200

    newdiv

--- html_maryarthur/script.js
+++ html_maryarthur/script.js
@@ -5,3 +5,9 @@ const NewElt= document.createElement("div");
 let elt = document.getElementById("conteneur");
 
 elt.appendChild(NewElt);
+
+const NewElt1= document.createElement("div");
+elt.appendChild(NewElt1);
+
+let elt1 = document.getElementById ("containeur");
+elt1.innerHTML = <ul><il>fisrt</il><il>second</il><il>last</il></ul>
