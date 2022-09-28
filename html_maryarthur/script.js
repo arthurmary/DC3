5110bcd1642f5738d90c00eb82441385ba5b1182
arthurmary <113183213+arthurmary@users.noreply.github.com>
2022-09-28 14:31:25 +0200

    div

--- html_maryarthur/script.js
+++ html_maryarthur/script.js
@@ -1,2 +1,7 @@
 const Element1 = document.getElementById("conteneur");
 console.log(Element1);
+
+const NewElt= document.createElement("div");
+let elt = document.getElementById("conteneur");
+
+elt.appendChild(NewElt);
